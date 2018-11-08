130206e13b30b7f95ef974582eb6a5562da47a94
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-11-08 09:37:47 -0700

    Working on bugfix for koan 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -6,8 +6,18 @@ data class MyDate(
   val dayOfMonth: Int
 ): Comparable<MyDate> {
   override fun compareTo(other: MyDate): Int {
-    val thisDate = "$year$month$dayOfMonth".toInt()
-    val otherDate = "${other.year}${other.month}${other.dayOfMonth}".toInt()
+    var formattedMonth = "$month"
+    var formattedDay = "$dayOfMonth"
+    var otherFormattedMonth = "${other.month}"
+    var otherFormattedDay = "${other.dayOfMonth}"
+
+    if (month < 10) formattedMonth = "0$month"
+    if (dayOfMonth < 10) formattedDay = "0$dayOfMonth"
+    if (other.month < 10) otherFormattedMonth = "0${other.month}"
+    if (other.dayOfMonth < 10) otherFormattedDay = "0${other.dayOfMonth}"
+
+    val thisDate = "$year$formattedMonth$formattedDay".toInt()
+    val otherDate = "${other.year}$otherFormattedMonth$otherFormattedDay".toInt()
 
     if (thisDate == otherDate) return 0
     if (thisDate < otherDate) return -1
@@ -24,3 +34,11 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun DateRange.contains(d: MyDate): Boolean {
+  val higherThanStart = start.compareTo(d)
+  val lowerThanEnd = endInclusive.compareTo(d)
+  val validRange = start.compareTo(endInclusive)
+  if (validRange == 1 && higherThanStart > -1 && lowerThanEnd < 1) return true
+  return false
+}
